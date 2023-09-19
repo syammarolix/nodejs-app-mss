9e8161bf5d9d11acb3741aa6a4eaac14dba7dcf7
syammarolix <129362298+syammarolix@users.noreply.github.com>
2023-09-20 01:07:26 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.94.111.10:9000/',
+  serverUrl: 'http://54.176.1.121:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_c04099b02f5b9be2fc1379498e5151f80ae64077',
+	    'sonar.login': 'squ_6a6ca776ddb0b967e23fdde5550a6c1fdfb7c81b',
 	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
+	    //'sonar.password': 'admin1',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
